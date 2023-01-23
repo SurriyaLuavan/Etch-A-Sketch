03d248fd3845e9f046487efae56669053a9617d4
SurriyaLuavan <surriyaluavan@gmail.com>
2023-01-23 11:38:57 -0400

    js functionality completed and verified

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ for(let j = 1; j <= number; j++) {
         let div = document.createElement('div');
         div.id = i + j ;
         div.classList.add('pixel');
-        div.setAttribute('style', `flex: 1; border: 2px solid; text-align: center;`)
+        div.setAttribute('style', `flex: 1; border: 2px solid; text-align: center; background-color: rgb(255,255,255)`)
         container2.append(div);
     }
     }
@@ -20,21 +20,35 @@ for(let j = 1; j <= number; j++) {
 
 function changePixelColor(e) {
     if (e.target.classList.contains('pixel')) {
-            e.target.classList.add('pixelHover');
+       let color =  e.target.style.backgroundColor;
+    //let styles = getComputedStyle(e.target);
+        if (color === 'rgb(255, 255, 255)') {
+            let r = Math.floor (Math.random() * 255);
+            let g = Math.floor (Math.random() * 255);
+            let b = Math.floor (Math.random() * 255);
+            e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            return;
+        }
+        let rgb = color.split("(")[1].split(")")[0].split(",").map(x => parseInt(x));
+        let rgbNew = rgb.map(color => (color - (color * 0.15)));
+        e.target.style.backgroundColor = `rgb(${rgbNew[0]}, ${rgbNew[1]}, ${rgbNew[2]})`;
     }
 }
 
 function selectGridSize(e) {
-    if(container.hasChildNodes) {
-    let children = container.children;
-    Array.from(children).forEach (child => {child.remove();});
-    let size = parseInt(prompt(`Enter a grid size (max: 100): `));
-    while (size > 100) {
-        size = prompt('Please enter a grid size less than 100!');
+    let size = prompt(`Enter a grid size (max: 50): `);
+    if(size === null){
+        return;
+    }
+    while (size > 50) {
+        size = prompt('Please enter a grid size less than 50!');
             if(size === null){
                 return;
         }
     }
+    if(container.hasChildNodes) {
+    let children = container.children;
+    Array.from(children).forEach (child => {child.remove();});
     gridCreate(size);
     }
 }
